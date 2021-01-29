54787ae5f6601fcd4c9e0324601ffbd64a50583f
vcoetoG <vcoeto@hotmail.com>
2021-01-29 15:36:21 -0600

    filter for mongo

--- api/Flask_mongo/main.py
+++ api/Flask_mongo/main.py
@@ -7,6 +7,7 @@ from bson import json_util
 cluster = MongoClient("mongodb+srv://Jorge:cGpoYxUlFA17JUOb@cluster0.yoqut.mongodb.net/ProyectoFinal?retryWrites=true&w=majority")
 db = cluster["ProyectoFinal"]
 collection = db["Gobierno"]
+Hospital = db["Hospital"]
 
 app = Flask(__name__)
 
@@ -14,3 +15,27 @@ app = Flask(__name__)
 def get_gobiernos():
     all_gobiernos = list(collection.find({}))
     return json.dumps(all_gobiernos, default=json_util.default)
+
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import json
+from bson import json_util
+
+
+
+@app.route('/gobiernos',methods = ['GET'])
+def get_gobiernos():
+    filt = {'_id': 1}
+    users = collection.find(filt)
+    output = [{'_id': user['_id'],
+    'Municipio': user['Municipio'],
+    'Username' : user['Username'],
+    'Password': user['Password'],
+    'Vacunas-Disp': user['Vacunas-Disp'],
+    'Vacunas-utl': user['Vacunas-utl']
+    }
+    for user in users
+    ]
+
+
+    return jsonify(output)
