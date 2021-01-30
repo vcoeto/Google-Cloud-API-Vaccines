acd4a5ccefdf69107c35006e51e3b5f274befe32
Tonojunco <tonojunco@gmail.com>
2021-01-30 14:30:51 -0600

    Se agrego front end y templates

--- api/Flask_mongo/main.py
+++ api/Flask_mongo/main.py
@@ -1,15 +1,75 @@
-from flask import Flask
+from flask import Flask, jsonify, request, redirect, render_template, url_for, session, redirect
 import pymongo 
 from pymongo import MongoClient
 import json
 from bson import json_util
-
+from flask_pymongo import PyMongo
+#Hash de password
+import bcrypt
+import os
 cluster = MongoClient("mongodb+srv://Jorge:cGpoYxUlFA17JUOb@cluster0.yoqut.mongodb.net/ProyectoFinal?retryWrites=true&w=majority")
 db = cluster["ProyectoFinal"]
 collection = db["Gobierno"]
 Hospital = db["Hospital"]
 
-app = Flask(__name__)
+
+template_dir = os.path.abspath('../templates')
+app = Flask(__name__,template_folder=template_dir)
+app.config["MONGO_URI"]="mongodb+srv://Jorge:cGpoYxUlFA17JUOb@cluster0.yoqut.mongodb.net/ProyectoFinal?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+##############################################################33
+#Front-end login
+
+#All the routing in our app will be mentioned here 
+#Proyecto final que se encuentra en el URI connect
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+    return render_template('index.html')
+#db_operations = mongo.db.<Collection_name>
+#do_operations = mongo.db.User
+
+
+#When you press login
+@app.route('/login',methods=['POST'])
+def login():
+    users=mongo.db.Gobierno
+    login_user = users.find_one({'Username': request.form['username']})
+
+    if login_user:
+        if bycrypt.hash(request.form['pass'].encode('utf-8'))
+#When you press register
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method=='POST':
+        #Check if it is registered already
+        users = mongo.db.Gobierno
+        #The 'username appears in the register.html. Pass username and look in database for name
+        #'name' referse to the name column in databae, may need to change that
+        existing_user = users.find_one({'name':request.form['username']})
+        #Check if the user does not exist and register it
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            #Insert in name username, and password the hash password
+            users.insert({'name':request.form['username'], 'password': hashpass})
+            session['username']=request.form['username']
+            return redirect(url_for('index'))
+        #Existing user was not none
+        return 'That username already exists!'
+    
+    return render_template('register.html')
+if __name__=='__main__':
+    app.secret_key='secretivekey'
+    app.run(debug=True)
+
+#End of frontend login
+##############################################################################
+
+
 
 @app.route("/gobierno",methods = ["GET"])
 def get_gobiernos():
